204ae2eac0586bfadca3e3286e118d7fae3ff944
lucastimho <lucas.t.ho.lh@gmail.com>
2021-11-22 13:03:05 -0600

    add note selector and noteeditor component

---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="note-selector">
+      <p class="note-selector-title">Second note...</p>
+      <p class="note-selector-timestamp">Timestamp here...</p>
+    </div>
+    <div class="note-selector">
+      <p class="note-selector-title">Third note...</p>
+      <p class="note-selector-timestamp">Timestamp here...</p>
+    </div>
+  </div>
+</template>
--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -4,13 +4,16 @@
       <p class="note-selector-title">First note...</p>
       <p class="note-selector-timestamp">Timestamp here...</p>
     </div>
-    <div class="note-selector">
-      <p class="note-selector-title">Second note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
-    <div class="note-selector">
-      <p class="note-selector-title">Third note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
+    <NoteSelector />
   </div>
 </template>
+
+<script>
+import NoteSelector from "./NoteSelector.vue";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+};
+</script>
